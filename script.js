9dc243f6508eba2dd5ba9c583bddbb26634f2127
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2020-10-31 00:53:37 +0530

    testing

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.onload = () => {
     const grid = document.querySelector('.grid');
     const msnry = new Masonry(grid, {
         itemSelector: '.grid-item',
-        gutter: 20,
         fitWidth: true,
         fitHeight: true
     });
